0d6c1ab4c3aab66ed32e2474dc3d2550fe3e3bac
srinathram17 <srinathram17@gmail.com>
2022-09-11 12:38:25 +0530

    Initial Project

---
+++ src/test/java/org/git/test/GitPractice.java
@@ -0,0 +1,10 @@
+package org.git.test;
+
+public class GitPractice {
+public static void main(String[] args) {
+	System.out.println("Test1");
+	System.out.println("Test2");
+	System.out.println("Test3");
+}
+	
+}
